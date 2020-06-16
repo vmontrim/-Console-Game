86301cba9536f9a51794ae46d0b6e5d3a5f972df
vmontrim <56654428+vmontrim@users.noreply.github.com>
2020-06-16 17:25:20 +0100

    Update Game.cs

--- Game.cs
+++ Game.cs
@@ -4,10 +4,8 @@ namespace ConsoleGame
 {
   class Game : SuperGame
   {
-  //This Method will update coordinate of character based on inputs
     public new static void UpdatePosition(string keyPressed, out int x, out int y)
         {
-            //First we set 2 variables in order to avoid errors
             x = 0;
             y = 0;
 
@@ -32,13 +30,10 @@ namespace ConsoleGame
             }
         }
 
-       //This method will allow the player icon to change with each keypress.
-       //Its input will be the key pressed and it will return a symbol
-       //that represents the player.
        public new static char UpdateCursor(string keyPressed)
         {
 
-            char direction = 'o'; // First we set a variable named direction 
+            char direction = 'o';
 
             switch(keyPressed)
             {
@@ -59,30 +54,27 @@ namespace ConsoleGame
                     break;
             }
 
-            return direction; //Finaly, we return the direction to the program
+            return direction;
         }
-
-        //Without this method, hitting the boundaries will break the game!
         public new static int KeepInBounds(int coordinate, int maxValue)
         {
             
 
             if (coordinate > maxValue)
             {
-                return 0; // If the player went to the left or top edge, send on the opposite side
+                return 0;
             }
             else if (coordinate < 0)
             {
-                return maxValue; // same
+                return maxValue;
             }
             else
             {
-                return coordinate; // It's OK
+                return coordinate;
             }
         }
 
 
-        //This method will compare coordinates and put score or not
         public new static bool DidScore(int charX, int charY, int fruitX, int fruitY)
         {
             if(charX == fruitX && charY == fruitY)
